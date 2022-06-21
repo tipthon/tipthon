44e03faf84c0ca58c2c7aaf76dc6c4d78299c793
tipthon <104599353+tipthon@users.noreply.github.com>
2022-06-21 15:34:50 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -1,9 +1,9 @@
 import re
 import time
 from datetime import datetime
-from userbot import StartTime, iqthon
-from userbot.Config import Config
-from userbot.plugins import mention
+from Arab import StartTime, iqthon
+from Arab.Config import Config
+from Arab.plugins import mention
 help1 = ("**☭⦙ كيفيه التنصيب :**")
 help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @E9N99\n**☭⦙ شـرح اوامـر السـورس : @a_aaaf**\n**☭⦙ شـرح فـارات السـورس : @aa_aaf** ")
 TG_BOT = Config.TG_BOT_USERNAME
