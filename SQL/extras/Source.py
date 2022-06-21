eda0099098a056734588287652d03458a0b0ec68
tipthon <104599353+tipthon@users.noreply.github.com>
2022-06-21 15:34:18 +0300

    Add files via upload

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -1,11 +1,11 @@
 import re
 import time
 from datetime import datetime
-from Arab import StartTime, iqthon
-from Arab.Config import Config
-from Arab.plugins import mention
-help1 = ("**♛ ⦙ كيفيه التنصيب :**")
-help2 = ("**♛ ⦙ قـائمـه الاوامـر :**\n**♛ ⦙ قنـاه السـورس :** @iqthon\n**♛ ⦙ شـرح اوامـر السـورس : @l3ll3**\n**♛ ⦙ شـرح فـارات السـورس : @teamtelethon** \n - اوامر الاونلاين تشتغل فقط في المجموعات ")
+from userbot import StartTime, iqthon
+from userbot.Config import Config
+from userbot.plugins import mention
+help1 = ("**☭⦙ كيفيه التنصيب :**")
+help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @E9N99\n**☭⦙ شـرح اوامـر السـورس : @a_aaaf**\n**☭⦙ شـرح فـارات السـورس : @aa_aaf** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**♛ : version 7.6  𓇡.** \n♛ : me  {mention}  𓇡. \n**♛ : time  {TM}  𓇡.**\n**♛ : My Bot {TG_BOT} 𓇡.**\n**♛ : Source : @iqthon  𓇡.**"
+Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source : @E9N99  𓇡.**"
