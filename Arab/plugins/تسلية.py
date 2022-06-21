a0bc4b283c0f116d9b47ff6f1e321e520899d0bb
tipthon <104599353+tipthon@users.noreply.github.com>
2022-06-21 15:24:59 +0300

    Update تسلية.py

--- Arab/plugins/تسلية.py
+++ Arab/plugins/تسلية.py
@@ -9,7 +9,7 @@ import math
 import base64
 import urllib
 from telethon.tl import functions
-from userbot.utils.decorators import register
+from Arab.utils.decorators import register
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.errors import ChatSendInlineForbiddenError, ChatSendStickersForbiddenError
@@ -23,20 +23,20 @@ from telethon.events import CallbackQuery
 from telethon.utils import get_display_name
 from collections import deque
 from random import choice
-from userbot import iqthon
+from Arab import iqthon
 from ..helpers import catmemes
 from ..core.managers import edit_or_reply, edit_delete
 from . import ALIVE_NAME
 from ..helpers import fonts as emojify
-from userbot.helpers.functions import convert_toimage,    deEmojify,    phcomment,    threats,    trap,    trash
-from userbot.helpers.functions import convert_tosticker,    flip_image,    grayscale,    invert_colors,    mirror_file,    solarize
+from Arab.helpers.functions import convert_toimage,    deEmojify,    phcomment,    threats,    trap,    trash
+from Arab.helpers.functions import convert_tosticker,    flip_image,    grayscale,    invert_colors,    mirror_file,    solarize
 from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
 from . import deEmojify
 from typing import Optional, Tuple
 from PIL import Image, ImageDraw, ImageFont
 import PIL.ImageOps
 from ..helpers import get_user_from_event
-from userbot.helpers.utils.tools import take_screen_shot
+from Arab.helpers.utils.tools import take_screen_shot
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     args = shlex.split(cmd)
     process = await asyncio.create_subprocess_exec(        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE    )
